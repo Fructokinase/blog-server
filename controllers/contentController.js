7e56af0ce069e5b162fa69caf41f26e9de8ef36e
Alex Chen <Alex.yf.Chen@outlook.com>
2016-01-22 20:19:45 -0500

    allow x origin header

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -13,12 +13,18 @@ contentController.getContentList = function (req, res) {
         data.result = result;
         data.message_code = 200;
         data.message = "ok";
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(200).json(data);
     })
     .catch(function (err) {
         data.message_code = 500;
         data.message = "Internal server error";
         data.error = err;
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(500).json(data);
     });
 }
@@ -37,13 +43,19 @@ contentController.postBlog = function (req, res) {
     .then(function (result) {
         data.result = result
         data.message_code = 200;
-        data.message = "ok"
+        data.message = "ok";
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(200).json(data);
     })
     .catch(function (err) {
         data.message_code = 500;
         data.message = "Internal server error";
         data.error = err;
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(500).json(data);
     });
 }
@@ -59,11 +71,17 @@ contentController.getPictureByBlogId = function (req, res) {
         data.result = result;
         data.message_code = 200;
         data.message = "ok";
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(200).json(data);
     })
     .catch(function (err) {
         data.message_code = 500;
         data.message = "Internal server error";
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.status(500).json(data);
     });
 }
